2fb61c35d2aba78859565cb72996f30fdc8fe1f6
Zaki-dev23 <122434294+Zaki-dev23@users.noreply.github.com>
2023-05-03 17:54:17 +0100

    Add files via upload

---
+++ src/Details.js
@@ -0,0 +1,175 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Zakaria Ait Laasri",
+  tagline: "Full-Stack devloper and UX & UI designer",
+  img: profile,
+  about: `As a UX/UI designer with full-stack expertise, I can create seamless user experiences across multiple platforms. My skills in UX/UI design and full-stack development allow me to understand user needs, design visually appealing interfaces, and translate designs into functional products. This results in a positive user experience for all who interact with the product.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/mwlite/in/zakaria-ait-laasri-941734262",
+  github: "https://www.github.com/Zaki-dev23",
+  twitter: "https://twitter.com/laasawi90844",
+  instagram: "https://www.instagram.com/zaki-lassri/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend Development",
+    Company: "Udemy, YouTube, Google, Medium",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "Jan 2022 - Present",
+  },
+  {
+    Position: "Bachelor in Electronics & Communication",
+    Company: `Your College Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "hi@example.com",
+  phone: "+91 12345 67890",
+};
